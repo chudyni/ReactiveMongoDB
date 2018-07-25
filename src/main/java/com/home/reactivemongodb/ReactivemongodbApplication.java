a2276a2bed40ed4042997ed162c20f8b6797470f
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2018-07-25 20:08:07 +0200

    Initialized app

---
+++ src/main/java/com/home/reactivemongodb/ReactivemongodbApplication.java
@@ -0,0 +1,12 @@
+package com.home.reactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivemongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivemongodbApplication.class, args);
+    }
+}
---
+++ src/test/java/com/home/reactivemongodb/ReactivemongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.home.reactivemongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactivemongodbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
